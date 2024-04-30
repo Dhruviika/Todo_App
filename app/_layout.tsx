81dd2a78df8e6bcc25df958f657143c4aefc0ee5
Dhruviika <E21CSEU0292@bennett.edu.in>
2024-04-30 22:24:46 +0530

    imporved ui of the welcome screen.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,7 @@ export default () => {
       <Stack.Screen
         name="index"
         options={{
+          headerShown: false,
           title: "Welcome",
         }}
       />
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, Pressable } from "react-native";
 import { useRouter } from "expo-router";
 import { useFonts } from "expo-font";
 
 const WelcomeScreen = () => {
   const router = useRouter();
+
   const [fontsLoaded] = useFonts({
     light: require("../assets/fonts/Inter-Light.ttf"),
     regular: require("../assets/fonts/Inter-Regular.ttf"),
     medium: require("../assets/fonts/Inter-Medium.ttf"),
     semibold: require("../assets/fonts/Inter-SemiBold.ttf"),
     bold: require("../assets/fonts/Inter-Bold.ttf"),
+    maven: require("../assets/fonts/MavenPro-Regular.ttf"),
   });
 
   if (!fontsLoaded) {
@@ -22,9 +24,43 @@ const WelcomeScreen = () => {
         justifyContent: "center",
         alignItems: "center",
         height: "100%",
+        paddingHorizontal: 50,
+        rowGap: 30,
       }}>
-      <Text style={{ fontFamily: "medium" }}>WELCOME</Text>
-      <Button title="Go to Home" onPress={() => router.push("/home")} />
+      <Text
+        style={{
+          fontFamily: "maven",
+          fontSize: 30,
+          textAlign: "center",
+        }}>
+        Manage Your Daily Todo
+      </Text>
+      <Text
+        style={{
+          fontFamily: "light",
+          fontSize: 18,
+          textAlign: "center",
+        }}>
+        With our app, you can effectively manage your time
+      </Text>
+      <Pressable
+        style={{
+          backgroundColor: "#FF6961",
+          alignSelf: "center",
+          borderRadius: 10,
+        }}
+        onPress={() => router.push("./home")}>
+        <Text
+          style={{
+            color: "white",
+            fontFamily: "medium",
+            fontSize: 20,
+            padding: 15,
+            textAlign: "center",
+          }}>
+          Get Started
+        </Text>
+      </Pressable>
     </View>
   );
 };
